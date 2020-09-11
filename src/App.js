37a88a6db85cc4a4b56b8b1d6c2021f8b8af09d6
Sagar Giri <sagargiri8846@gmail.com>
2020-09-11 19:43:01 +0530

    homepage and menu successfull

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Homepage from './pages/homepage/homepage.component';
+import './App.css'
+function App() {
+
+  return (<>
+    <Homepage/>
+  </>)
+}
+
+export default App;
---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MenuItem from '../menu-item/menu-item-component';
+import './directory.style.scss'
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sections: [{
+                title: 'hats',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id:1
+            },{
+                title: 'jackets',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id:2
+            },{
+                title: 'sneakers',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                id:3
+            },{
+                title: 'womens',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    id: 4,
+                size:'large'
+            },{
+                title: 'mens',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',    
+                id: 5,
+                size:'large'
+            },]
+        }
+    }
+
+    componentWillMount() {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillReceiveProps(nextProps) {
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                        
+                <div className="directory-menu">
+                    {
+                        this.state.sections.map(({id,title,imageUrl,size}) => (
+                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/> 
+                        ))
+                    }
+                
+                
+            </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default Directory;
